bab9a70dc4cb06d39aa92f4815ee889839f64f95
choisungjin <goodsungjin.dev@gmail.com>
2022-01-07 02:37:43 +0900

    빠진 숫자 찾기

---
+++ leet_code/268_Missing_Number.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/missing-number/submissions/
+
+class Solution:
+  def missingNumber(self, nums: List[int]) -> int:
+    return int(len(nums) * (len(nums) + 1) / 2) - sum(nums)
